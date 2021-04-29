47dab35a497ef7481e2bce16aaa1f91f311144ed
Solidus1986 <54433037+Solidus1986@users.noreply.github.com>
2021-04-29 10:58:23 +0200

    add last video

--- src/components/chat/Responses.js
+++ src/components/chat/Responses.js
@@ -91,7 +91,7 @@ export default function Responses({dataResponses, dataHandleSubmit}) {
                                     return <div className="video"> <Player  src="https://www.hennessy.com/sites/hennessy/files/2020-04/the_hennessy_north_star_cocktail.mp4"  fluid={false} width={320} height={190}><BigPlayButton position="center" /></Player></div>;
                                 case 'URL_COCKTAIL:Cocktail Sazerac':
                                     return <div className="video"> <Player  src="https://www.hennessy.com/sites/hennessy/files/2021-03/hennessy-cocktails-tutorial-1440x770-24-sazerac.mp4"  fluid={false} width={320} height={190}><BigPlayButton position="center" /></Player></div>;
-                                case 'URL_COCKTAIL:Cocktail Aritochat-bot':
+                                case 'URL_COCKTAIL:Cocktail Aritochat_bot':
                                     return <div className="video"> <Player  src="https://www.hennessy.com/sites/hennessy/files/2020-04/hennessy_aristocrat_cocktail.mp4"  fluid={false} width={320} height={190}><BigPlayButton position="center" /></Player></div>;
                                     
                                default:
